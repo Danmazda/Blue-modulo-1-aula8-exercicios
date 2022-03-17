91a99ce96b5a968f194cd04dbfcf69ad826670c2
Danilo Procopio <dan_mazda@hotmail.com>
2022-03-17 12:47:52 -0300

    ex1 feito

---
+++ ex1.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+const userCols = +prompt("Digite quantas  COLUNAS deseja na matriz: ");
+const userRows = +prompt("Digite quantas LINHAS deseja na matriz:");
+const cols = [];
+let rows = [];
+for (let indexCol = 1; indexCol <= userCols; indexCol++) {
+  // linhas:
+  for (let indexLine = 1; indexLine <= userRows; indexLine++) {
+    rows.push(indexLine);
+  }
+  cols.push(rows);
+  rows = [];
+}
+
+console.log("Matriz: ");
+cols.forEach((line) => console.log(line));
